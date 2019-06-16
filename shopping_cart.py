2ad2cd22f5d3d3758279f6654ac2bcfabc906d77
Eileen Hengel <eileenhengel@Bobs-MacBook-Pro.local>
2019-06-16 16:39:07 -0400

    Add gspread and update code

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,57 +2,38 @@
 
 from pprint import pprint
 import datetime
+import gspread
+import pandas as pd
+import numpy as np
 
-products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+from oauth2client.service_account import ServiceAccountCredentials
 
-#print(products)
-#pprint(products)
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('shoppingcart-243815-d4ac18c1b8aa.json', scope)
+client = gspread.authorize(creds)
+
+# Find a shopping cart worksheet by name and open the first sheet
+sheet = client.open("Shopping Cart Project - Products Database").sheet1
+
+#extract data as list and convert to dataframe
+df = pd.DataFrame(sheet.get_all_values(), columns=['id','name','aisle','department','price'])
+
+#convert price from object to string
+df["price"]= df["price"].astype(float)
 
 #define variables
-
 product_ids = []
 total_price = 0
 user_input = '' #string
 now = datetime.datetime.now()
 tax_usd = 0
 
-
 #define functions
 def to_usd(price):
     price_usd = "${:,.2f}".format(price)
     return price_usd
-
-#Define valid inputs
-
-prod = []
-valid_prod = set
-
-for p in products:
-    if p["id"] not in prod:
-        prod.append(p["id"])
-
-    valid_prod = str(set(prod))
 
 #Capture Inputs
 
@@ -60,22 +41,24 @@ while user_input is not "DONE":
     #ask user for product
     user_input = input("Please input a product identifier:")
 
-    #validate input
-    if user_input in valid_prod:
+    #validate input and handel errors
+    if df['id'].isin([user_input]).any():
         product_ids.append(user_input) 
     elif user_input == "DONE":
         break
     elif user_input == "LIST":
-        print(valid_prod)
+        print(list(df['id']))
     else:   
         print(user_input + " is an invalid product code. Please input valid product code. When complete, type DONE. For list of product codes type LIST")
-    
-print(product_ids)
 
-#print inputs
+#print inputs 
+print(product_ids)
 
 print("---------------------------------")
 print("THE NEW STORE")
+print("51 WEST 4TH STREET")
+print("NEW YORK, NY 10003")
+print("212-555-5555")
 print("WWW.THENEWSTORE.COM")
 print("---------------------------------")
 print("CHECKOUT AT: " + str(now.strftime("%Y-%m-%d %H:%M %p")))
@@ -83,17 +66,17 @@ print("---------------------------------")
 print("SELECTED PRODCUTS:")
 
 #print selected products
-
 for i in product_ids:
-    matching_product = [p for p in products if str(p["id"]) == str(i)]
-    matching_product = matching_product[0]
-    price_usd = to_usd(matching_product["price"])   #convert price to USD
-    total_price = total_price + matching_product["price"]
-    print("... " + matching_product["name"] + " (" + str(price_usd) + ")")
-
+    #matching_product = [p for p in products if str(p["id"]) == str(i)]
+    matching_product = str(df.loc[df['id'] == str(i)]['name'].values)
+    matching_product = matching_product.strip("['']")
+    price = float(df.loc[df['id'] == str(i)]['price'])
+    price_usd = to_usd(price)  #convert price to USD
+    total_price = total_price + price
+    print("... " + matching_product + " (" + str(price_usd) + ")")
+ 
 #define tax and sub total
-
-tax_usd = total_price * 0.0874
+tax_usd = total_price * 0.0875
 sub_usd = total_price + tax_usd
 tax_usd = to_usd(tax_usd)
 sub_usd = to_usd(sub_usd)
